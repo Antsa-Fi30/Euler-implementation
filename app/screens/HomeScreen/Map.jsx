d87ecb2e499fa3655cec536017ca2adc15feac71
Fabyay <mariefabia.laporte@gmail.com>
2024-04-28 18:37:22 +0300

    ...

--- app/screens/HomeScreen/Map.jsx
+++ app/screens/HomeScreen/Map.jsx
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Color from "../../utils/Colors";
+import MapView from "react-native-maps";
 
 export default function Map() {
   return (
     <View>
-      <Text style={styles.title}>Map ici</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -18.87019,
+          longitude: 47.507905,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
@@ -13,5 +22,9 @@ export default function Map() {
 const styles = StyleSheet.create({
   title: {
     color: Color.light.primary,
+  },
+  map: {
+    height: "100%",
+    width: "100%",
   },
 });
